b281d312760f66f6967359d585f3a98b218007af
Kiresh26 <kiresh26@gmail.com>
2022-07-09 21:25:24 +0200

    ???

--- main.h
+++ main.h
@@ -5,12 +5,4 @@
 
 int _putchar(int c);
 int _printf(const char *format, ...);
-int _print_str(char *string);
-int _print_int(long int var);
-int to_Binary(unsigned int n);
-int to_Octal(unsigned int n);
-int _power_recursion(int x, int y);
-int to_Hexa(unsigned int num);
-
-
 #endif /*MAIN_H*/
