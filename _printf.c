b753332bbc3ea97b426268fd62d70956e73f1fcf
Kiresh26 <kiresh26@gmail.com>
2022-07-09 21:09:10 +0200

    Test

--- _printf.c
+++ _printf.c
@@ -1,55 +1,80 @@
 #include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-#include <stddef.h>
+#include <stdio.h>
 
 /**
- * _printf - recreates the printf func
- * @format: string with format specifier
- * Return: Number of Char printed
- *
- */
+  * _printf - produces output according to a format.
+  * @format: a character string.
+  * Return: number of characters printed(
+  * excluding the null terminator)
+  */
 
 int _printf(const char *format, ...)
 {
-	if (format != NULL)
-	{
-		int count = 0, i;
-		int (*m)(va_list);
-		va_list args;
+	int count;
+	int total = 0;
+	va_list args;
+	int flag = 0;
 
-		va_start(args, format);
-		i = 0;
-		if (format[0] == '%' && format[1] == '\0')
-			return (-1);
-		while (format != NULL && format[i] != '\0')
+	if (format == NULL)
+		return (0);
+
+	va_start(args, format);
+	for (count = 0; *(format + count) != '\0'; count++)
+	{
+		if (format[count] == '%')
 		{
-			if (format[i] == '%')
+			flag = 1;
+		}
+		else if (flag == 1)
+		{
+			flag = 0;
+			switch (format[count])
 			{
-				if (format[i + 1] == '%')
-				{
-					count += _putchar(format[i]);
-					i += 2;
-				}
-				else
-				{
-					m = get_func(format[i + 1]);
-					if (m)
-						count += m(args);
-					else
-						count = _putchar(format[i]) + _putchar(format[i + 1]);
-					i += 2
-				}
-			}
-			else
-			{
-				count += _putchar(format[i]);
-				i++
+				case 'c':
+					_putchar(va_arg(args, int));
+					total += 1;
+					break;
+				case 's':
+					total += _print_str(va_arg(args, char *));
+					break;
+				case '%':
+					_putchar('%');
+					total += 1;
+					break;
+				case 'd':
+					total += _print_int((long)(va_arg(args, int)));
+					break;
+				case 'i':
+					total += _print_int((long)(va_arg(args, int)));
+					break;
+				case 'b':
+					total += to_Binary(va_arg(args, int));
+					break;
+				case 'u':
+					total += _print_int(va_arg(args, unsigned int));
+					break;
+				case 'o':
+					total += to_Octal(va_arg(args, int));
+					break;
+				case 'x':
+					total += to_Hexa(va_arg(args, int));
+					break;
+				case 'X':
+					total += to_Hexa(va_arg(args, int));
+					break;
+				default:
+					_putchar('%');
+					_putchar(format[count]);
+					total += 2;
 			}
 		}
-		va_end(args);
-		return (count);
+		else
+		{
+			_putchar(format[count]);
+			total += 1;
+		}
 	}
-	return (-1);
+	va_end(args);
+	return (total);
 }
--- main.h
+++ main.h
@@ -1,14 +1,16 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 #include <stdarg.h>
 
 int _putchar(int c);
 int _printf(const char *format, ...);
-int print_c(va_list args);
-int print_s(va_list args);
-int (*get_func(char x))(va_list args);
-int print_percent(va_list args);
+int _print_str(char *string);
+int _print_int(long int var);
+int to_Binary(unsigned int n);
+int to_Octal(unsigned int n);
+int _power_recursion(int x, int y);
+int to_Hexa(unsigned int num);
 
 
 #endif /*MAIN_H*/
