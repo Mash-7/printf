1612f07195c4901fcd38d09687011376f5a53dc4
Kiresh26 <kiresh26@gmail.com>
2022-07-09 20:46:42 +0200

    header update

--- main.h
+++ main.h
@@ -8,6 +8,7 @@ int _printf(const char *format, ...);
 int print_c(va_list args);
 int print_s(va_list args);
 int (*get_func(char x))(va_list args);
+int print_percent(va_list args);
 
 
 #endif /*MAIN_H*/
