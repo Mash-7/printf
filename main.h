05174859263fce323bbd7ebb69efbc1efe2bd813
Kireshchand Dhanasar <kiresh26@gmail.com>
2022-07-09 11:31:03 -0700

    _printf

--- main.h
+++ main.h
@@ -1,6 +1,13 @@
 #ifndef _MAIN_H
 #define _MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
 
 int _putchar(int c);
+int _printf(const char *format, ...);
+int print_c(va_list args);
+int print_s(va_list args);
+int (*get_func(char x))(va_list args);
+
 
 #endif /*MAIN_H*/
