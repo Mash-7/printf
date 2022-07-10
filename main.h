6ce673c292a52a73752659a335fc82b547bdc1f3
Mash-7 <mashoratile@gmail.com>
2022-07-10 12:20:33 -0700

    main.h

--- main.h
+++ main.h
@@ -11,7 +11,7 @@ int print_int(va_list arg);
 int print_dec(va_list arg);
 int print_rev(va_list arg);
 int print_bin(va_list arg);
-int print_unsig(va_list arg);
+int print_unsign(va_list arg);
 int print_octal(va_list arg);
 int print_hex(va_list arg);
 int print_rot13(va_list arg);
