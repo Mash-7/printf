fb7e79fbd66b71b16c4ddc8c62f5d8eedde0bc64
Kiresh26 <kiresh26@gmail.com>
2022-07-11 00:38:58 +0200

    update

--- _printf.c
+++ _printf.c
@@ -98,16 +98,3 @@ int _putstr(char *str)
 	return (out);
 }
 
-/**
- * _putchar - writes the character c to the buffer
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write_to_buffer(c, 0));
-}
-
-}
