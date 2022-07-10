7974f41c9141a12688d7c8557d6e09d3256cfd76
Mash-7 <mashoratile@gmail.com>
2022-07-09 21:33:06 -0700

    alt_main.h

--- main.h
+++ main.h
@@ -1,21 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdio.h>
 #include <stdarg.h>
-#include <unistd.h>
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-int print_string(va_list arg);
 int print_char(va_list arg);
-int print_dec(va_list arg);
+int print_string(va_list arg);
 int print_int(va_list arg);
-int print_rot13(va_list arg);
-int print_bin(va_list arg);
+int print_dec(va_list arg);
 int print_rev(va_list arg);
-int print_p(va_list arg);
-int print_unsign(va_list arg);
-int print_hex(va_list arg);
+int print_bin(va_list arg);
+int print_unsig(va_list arg);
 int print_octal(va_list arg);
+int print_hex(va_list arg);
+int print_rot13(va_list arg);
 
 #endif
