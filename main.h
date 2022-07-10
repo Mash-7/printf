314272e9076e026b4d3da3682e971203e6e4149c
Kiresh26 <kiresh26@gmail.com>
2022-07-10 23:26:58 +0200

    Header file update

--- main.h
+++ main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-#include <stdarg.h>
 
-int _printf(const char *format, ...);
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
+int _printf(const char *format, ...);
 int print_char(va_list c);
 int print_string(va_list s);
 int print_int(va_list i);
@@ -22,4 +24,4 @@ typedef struct code_format
 	int (*f)(va_list);
 } code_f;
 
-#endif /*MAIN_H*/
+#endif
